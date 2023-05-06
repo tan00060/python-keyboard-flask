c77e99785d2f406f7f5bce108ceac9cc82408c69
Ming <tan00060@algonquinlive.com>
2023-05-06 17:12:27 -0400

    added routes for switches with id

--- app/api/switch_routes.py
+++ app/api/switch_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from ..api_calls.switch_calls import get_all_switch, create_switch
+from ..api_calls.switch_calls import get_all_switch, create_switch, delete_switch, get_switch_by_id
 
 switch_routes = Blueprint('switch', __name__)
 
@@ -14,8 +14,26 @@ def switch():
 @switch_routes.route('/switch', methods=["POST"])
 def create_new_switch():
     if request.method == "POST":
-        print(request.json)
-        create_switch(request.json)
-        return "ctreate"
+        res = create_switch(request.json)
+        print(res)
+        return res
+    else:
+        None
+
+@switch_routes.route('/switch/<switch_id>', methods=["GET"])
+def get_by_id_switch(switch_id):
+    if request.method == "GET":
+        res = get_switch_by_id(switch_id)
+        print(res)
+        return res
+    else:
+        None
+        
+@switch_routes.route('/switch/<switch_id>', methods=["DELETE"])
+def delete_by_id_switch(switch_id):
+    if request.method == "DELETE":
+        res = delete_switch(switch_id)
+        print(res)
+        return res
     else:
         None
--- app/api_calls/switch_calls.py
+++ app/api_calls/switch_calls.py
@@ -21,11 +21,52 @@ def get_all_switch():
 def create_switch(data):
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
-            # curs.execute(f'INSERT INTO {table}{columns} VALUES{values};')
-            curs.execute("""
-                         INSERT INTO switch (name, switch_type_id)
-                         VALUES (%(name)s, %(switch_type_id)s)
-                         """,
-                         {'name': data["name"],
-                        'switch_type_id': data["switch_type_id"]
-                         })
+            try:
+                curs.execute("""
+                            INSERT INTO switch (name, switch_type_id)
+                            VALUES (%(name)s, %(switch_type_id)s)
+                            """,
+                            {'name': data["name"],
+                            'switch_type_id': data["switch_type_id"]
+                            })
+                return "Created", 200
+            except Exception as e:
+                return "Failed to create switch", 400
+
+
+def get_switch_by_id(switch_id):
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            try:
+                curs.execute("""
+                            SELECT id, name, switch_type_id FROM switch WHERE ID = (%(switch_id)s)
+                            """,
+                            {
+                            'switch_id': switch_id
+                            })
+                res = curs.fetchall()
+                if len(res):
+                    return res
+                else:
+                    return "Failed to find switch", 404
+            except Exception as e:
+                return "Failed to create switch", 400
+
+def delete_switch(switch_id):
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            try:
+                res = get_switch_by_id(switch_id)
+                print(res)
+                if res[-1] == 404:
+                    return "Failed to delete switch", 400
+                else:
+                    curs.execute("""
+                                DELETE FROM switch WHERE ID = (%(switch_id)s)
+                                """,
+                                {
+                                'switch_id': switch_id
+                                })
+                    return "Deleted Switch", 200
+            except Exception as e:
+                return "Failed to create switch", 400
