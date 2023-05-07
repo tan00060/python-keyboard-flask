34e7310d9f5e07e9ec0b3dc13680098e4259181a
Ming <tan00060@algonquinlive.com>
2023-05-07 15:13:22 -0400

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.config import Config
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from .models.db import db
 from .api.keyboard_routes import keyboard_routes
@@ -13,6 +14,7 @@ from .api.switch_type_routes import switch_type_routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)
 
 app.register_blueprint(keyboard_routes, url_prefix="/api")
 app.register_blueprint(keyboard_type_routes, url_prefix="/api")
